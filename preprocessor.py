0639efb9f49da37932c664ee562d3929d9c8dc97
Prem satpute <159188315+premsatpute@users.noreply.github.com>
2024-06-13 02:18:27 +0530

    Update preprocessor.py

--- preprocessor.py
+++ preprocessor.py
@@ -1,29 +1,27 @@
-
 import pandas as pd
 import re
 
-
-
-def preprocess (data ):
+def preprocess(data):
     # Define the pattern to match date-time format
     pattern = '\d{2}/\d{2}/\d{2},\s\d{1,2}:\d{2}\s[ap]m'
 
     # Split the data into messages and dates
     messages = re.split(pattern, data)[1:]
-
     dates = re.findall(pattern, data)
 
+    # Create a DataFrame with messages and date_time
     chat = pd.DataFrame({'messages': messages, 'date_time': dates})
 
-    # Convert the series to a datetime column, ensure format matches
+    # Convert the date_time column to datetime format
     chat['date_time'] = pd.to_datetime(chat['date_time'], format='%d/%m/%y, %I:%M %p')
 
-    # Format the datetime column to the desired format
+    # Create a 12-hour format date_time column
     chat['date_time_12_hrs'] = chat['date_time'].dt.strftime('%d/%m/%y, %I:%M %p')
 
     users = []
     messages = []
 
+    # Iterate through messages to extract users and message content
     for msg in chat['messages']:
         # Check if the message contains ':', indicating a user name
         if ':' in msg:
@@ -38,34 +36,32 @@ def preprocess (data ):
     # Add users and messages as new columns to the DataFrame
     chat['users'] = users
     chat['messages'] = messages
-    # separating year , month , day , hour and storing them to new column
 
-
+    # Add additional columns for year, month, day, hour, minute, month number, and day name
     chat['year'] = chat['date_time'].dt.year
     chat['month'] = chat['date_time'].dt.month_name()
     chat['hour'] = chat['date_time'].dt.hour
     chat['minute'] = chat['date_time'].dt.minute
     chat['month_num'] = chat['date_time'].dt.month
     chat['date'] = chat['date_time'].dt.date
-    chat['day_name']=chat['date_time'].dt.day_name()
+    chat['day_name'] = chat['date_time'].dt.day_name()
 
-    # heatmap
+    # Create period column for heatmap
     period = []
-
     for hour in chat['hour']:
         if hour == 23:
-            period.append(str(hour) + "-" + str("00"))
+            period.append(f"{hour}-00")
         elif hour == 0:
-            period.append(str("00") + "-" + str(hour + 1))
+            period.append("00-1")
         else:
-            period.append(str(hour) + "-" + str(hour + 1))
+            period.append(f"{hour}-{hour + 1}")
     chat['period'] = period
-    chat_new_order = ['messages', 'users', 'date_time_12_hrs', 'date_time',"date", 'year', 'month', 'hour', 'minute',
-                      'month_num','day_name','period']
+
+    # Reorder columns in the DataFrame
+    chat_new_order = [
+        'messages', 'users', 'date_time_12_hrs', 'date_time', 'date', 'year', 
+        'month', 'hour', 'minute', 'month_num', 'day_name', 'period'
+    ]
     chat = chat[chat_new_order]
 
     return chat
-
-
-
-
