a68f9c3473964c68b07eee7488f242c944bc1dee
Prem satpute <159188315+premsatpute@users.noreply.github.com>
2024-06-13 01:51:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -191,18 +191,7 @@ if uploaded_file is not None:
         st.divider()
         st.markdown('<h1 style="color: #FF6347;">Emoji Analysis</h1>', unsafe_allow_html=True)
 
-        # emoji analysis
-        emoji_df = helper.emoji_helper(selected_user, df)
-
-        col1, col2, col3 = st.columns(3)
-
-        with col2:
-            st.dataframe(emoji_df.head(10))
-        with col1:
-            fig, ax = plt.subplots(figsize=(4, 4))
-            colors = sns.color_palette("viridis", len(emoji_df.head()))
-            ax.pie(emoji_df[1].head(), labels=emoji_df[0].head(), autopct="%0.2f", colors=colors)
-            st.pyplot(fig)
+      
 
 
 
