664e1934dbf82791f8752d42d4633e0253da35e0
Prem satpute <159188315+premsatpute@users.noreply.github.com>
2024-06-12 23:57:05 +0530

    Update preprocessor.py

--- preprocessor.py
+++ preprocessor.py
@@ -1,51 +1,71 @@
-import re
+
 import pandas as pd
+import re
 
-def preprocess(data):
-    pattern = '\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}\s-\s'
 
+
+def preprocess (data ):
+    # Define the pattern to match date-time format
+    pattern = '\d{2}/\d{2}/\d{2},\s\d{1,2}:\d{2}\s[ap]m'
+
+    # Split the data into messages and dates
     messages = re.split(pattern, data)[1:]
+
     dates = re.findall(pattern, data)
 
-    df = pd.DataFrame({'user_message': messages, 'message_date': dates})
-    # convert message_date type
-    df['message_date'] = pd.to_datetime(df['message_date'], format='%d/%m/%Y, %H:%M - ')
+    chat = pd.DataFrame({'messages': messages, 'date_time': dates})
 
-    df.rename(columns={'message_date': 'date'}, inplace=True)
+    # Convert the series to a datetime column, ensure format matches
+    chat['date_time'] = pd.to_datetime(chat['date_time'], format='%d/%m/%y, %I:%M %p')
+
+    # Format the datetime column to the desired format
+    chat['date_time_12_hrs'] = chat['date_time'].dt.strftime('%d/%m/%y, %I:%M %p')
 
     users = []
     messages = []
-    for message in df['user_message']:
-        entry = re.split('([\w\W]+?):\s', message)
-        if entry[1:]:  # user name
-            users.append(entry[1])
-            messages.append(" ".join(entry[2:]))
+
+    for msg in chat['messages']:
+        # Check if the message contains ':', indicating a user name
+        if ':' in msg:
+            # Split the message into user name and actual message
+            user, message_text = msg.split(':', 1)
+            users.append(user.strip())  # Add the user name to the users list
+            messages.append(message_text.strip())  # Add the message to the messages list
         else:
-            users.append('group_notification')
-            messages.append(entry[0])
+            users.append('group notification')  # Label as group notification if ':' is not present
+            messages.append(msg.strip())  # Add the message to the messages list
 
-    df['user'] = users
-    df['message'] = messages
-    df.drop(columns=['user_message'], inplace=True)
+    # Add users and messages as new columns to the DataFrame
+    chat['users'] = users
+    chat['messages'] = messages
+    # separating year , month , day , hour and storing them to new column
 
-    df['only_date'] = df['date'].dt.date
-    df['year'] = df['date'].dt.year
-    df['month_num'] = df['date'].dt.month
-    df['month'] = df['date'].dt.month_name()
-    df['day'] = df['date'].dt.day
-    df['day_name'] = df['date'].dt.day_name()
-    df['hour'] = df['date'].dt.hour
-    df['minute'] = df['date'].dt.minute
 
+    chat['year'] = chat['date_time'].dt.year
+    chat['month'] = chat['date_time'].dt.month_name()
+    chat['hour'] = chat['date_time'].dt.hour
+    chat['minute'] = chat['date_time'].dt.minute
+    chat['month_num'] = chat['date_time'].dt.month
+    chat['date'] = chat['date_time'].dt.date
+    chat['day_name']=chat['date_time'].dt.day_name()
+
+    # heatmap
     period = []
-    for hour in df[['day_name', 'hour']]['hour']:
+
+    for hour in chat['hour']:
         if hour == 23:
-            period.append(str(hour) + "-" + str('00'))
+            period.append(str(hour) + "-" + str("00"))
         elif hour == 0:
-            period.append(str('00') + "-" + str(hour + 1))
+            period.append(str("00") + "-" + str(hour + 1))
         else:
             period.append(str(hour) + "-" + str(hour + 1))
+    chat['period'] = period
+    chat_new_order = ['messages', 'users', 'date_time_12_hrs', 'date_time',"date", 'year', 'month', 'hour', 'minute',
+                      'month_num','day_name','period']
+    chat = chat[chat_new_order]
 
-    df['period'] = period
+    return chat
 
-    return df
+
+
+
